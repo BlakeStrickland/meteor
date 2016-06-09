15ba80bb0570c3e2604b9d2c4d0d6a2a22811db1
Blake Strickland <Stricklandb02@gmail.com>
2016-06-09 15:49:06 -0400

    Add template

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -1,1 +1,14 @@
 PlayersList = new Mongo.Collection('players');
+
+Template.leaderboard.helpers({
+  'player': function(){
+    return PlayersList.find()
+  },
+  'otherHelperFunction': function(){
+    return "Some other function"
+  }
+});
+
+if(Meteor.isClient){
+console.log("Hello client");
+}
