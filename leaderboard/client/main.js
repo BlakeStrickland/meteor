8e807546eb8d8ed90580a9fb73857f9c29bb730d
Blake Strickland <Stricklandb02@gmail.com>
2016-06-10 15:10:38 -0400

    Add session

--- leaderboard/client/main.js
+++ leaderboard/client/main.js
@@ -1,18 +1,26 @@
+
 PlayersList = new Mongo.Collection('players');
 
 Template.leaderboard.helpers({
   'player': function(){
     return PlayersList.find()
   },
-  'otherHelperFunction': function(){
-    return "Some other function"
+  'selectedClass': function(){
+    var playerId = this._id;
+    var selectedPlayer = Session.get('selectedPlayer');
+    if(playerId == selectedPlayer){
+      return "selected"
+    }
   }
 });
 
 if(Meteor.isClient){
   Template.leaderboard.events({
     'click .player': function(){
-      console.log("You clicked an li element");
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(playerId);
     }
   });
 }
